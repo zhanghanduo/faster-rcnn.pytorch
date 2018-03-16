445e11268f1fc632d24e89a205fda6e1d7941dd3
zhanghanduo <hzhang032@e.ntu.edu.sg>
2018-03-16 13:02:10 +0800

    :construction: substitute resnet with kervnet, next step is to substitute RPN and roi pooling

--- lib/model/faster_rcnn/faster_rknn.py
+++ lib/model/faster_rcnn/faster_rknn.py
@@ -6,7 +6,6 @@ from torch.autograd import Variable
 import torchvision.models as models
 from torch.autograd import Variable
 import numpy as np
-from model.kervnet.kervolution import kerv
 from model.utils.config import cfg
 from model.rpn.rpn import _RPN
 from model.roi_pooling.modules.roi_pool import _RoIPooling
@@ -25,17 +24,17 @@ class _fasterRKNN(nn.Module):
         self.n_classes = len(classes)
         self.class_agnostic = class_agnostic
         # loss
-        self.RCNN_loss_cls = 0
-        self.RCNN_loss_bbox = 0
+        self.RKNN_loss_cls = 0
+        self.RKNN_loss_bbox = 0
 
         # define rpn
-        self.RCNN_rpn = _RPN(self.dout_base_model)
-        self.RCNN_proposal_target = _ProposalTargetLayer(self.n_classes)
-        self.RCNN_roi_pool = _RoIPooling(cfg.POOLING_SIZE, cfg.POOLING_SIZE, 1.0/16.0)
-        self.RCNN_roi_align = RoIAlignAvg(cfg.POOLING_SIZE, cfg.POOLING_SIZE, 1.0/16.0)
+        self.RKNN_rpn = _RPN(self.dout_base_model)
+        self.RKNN_proposal_target = _ProposalTargetLayer(self.n_classes)
+        self.RKNN_roi_pool = _RoIPooling(cfg.POOLING_SIZE, cfg.POOLING_SIZE, 1.0/16.0)
+        self.RKNN_roi_align = RoIAlignAvg(cfg.POOLING_SIZE, cfg.POOLING_SIZE, 1.0/16.0)
 
         self.grid_size = cfg.POOLING_SIZE * 2 if cfg.CROP_RESIZE_WITH_MAX_POOL else cfg.POOLING_SIZE
-        self.RCNN_roi_crop = _RoICrop()
+        self.RKNN_roi_crop = _RoICrop()
 
     def forward(self, im_data, im_info, gt_boxes, num_boxes):
         batch_size = im_data.size(0)
@@ -45,14 +44,14 @@ class _fasterRKNN(nn.Module):
         num_boxes = num_boxes.data
 
         # feed image data to base model to obtain base feature map
-        base_feat = self.RCNN_base(im_data)
+        base_feat = self.RKNN_base(im_data)
 
         # feed base feature map tp RPN to obtain rois
-        rois, rpn_loss_cls, rpn_loss_bbox = self.RCNN_rpn(base_feat, im_info, gt_boxes, num_boxes)
+        rois, rpn_loss_cls, rpn_loss_bbox = self.RKNN_rpn(base_feat, im_info, gt_boxes, num_boxes)
 
         # if it is training phrase, then use ground trubut bboxes for refining
         if self.training:
-            roi_data = self.RCNN_proposal_target(rois, gt_boxes, num_boxes)
+            roi_data = self.RKNN_proposal_target(rois, gt_boxes, num_boxes)
             rois, rois_label, rois_target, rois_inside_ws, rois_outside_ws = roi_data
 
             rois_label = Variable(rois_label.view(-1).long())
@@ -75,19 +74,19 @@ class _fasterRKNN(nn.Module):
             # pooled_feat_anchor = _crop_pool_layer(base_feat, rois.view(-1, 5))
             grid_xy = _affine_grid_gen(rois.view(-1, 5), base_feat.size()[2:], self.grid_size)
             grid_yx = torch.stack([grid_xy.data[:,:,:,1], grid_xy.data[:,:,:,0]], 3).contiguous()
-            pooled_feat = self.RCNN_roi_crop(base_feat, Variable(grid_yx).detach())
+            pooled_feat = self.RKNN_roi_crop(base_feat, Variable(grid_yx).detach())
             if cfg.CROP_RESIZE_WITH_MAX_POOL:
                 pooled_feat = F.max_pool2d(pooled_feat, 2, 2)
         elif cfg.POOLING_MODE == 'align':
-            pooled_feat = self.RCNN_roi_align(base_feat, rois.view(-1, 5))
+            pooled_feat = self.RKNN_roi_align(base_feat, rois.view(-1, 5))
         elif cfg.POOLING_MODE == 'pool':
-            pooled_feat = self.RCNN_roi_pool(base_feat, rois.view(-1,5))
+            pooled_feat = self.RKNN_roi_pool(base_feat, rois.view(-1,5))
 
         # feed pooled features to top model
         pooled_feat = self._head_to_tail(pooled_feat)
 
         # compute bbox offset
-        bbox_pred = self.RCNN_bbox_pred(pooled_feat)
+        bbox_pred = self.RKNN_bbox_pred(pooled_feat)
         if self.training and not self.class_agnostic:
             # select the corresponding columns according to roi labels
             bbox_pred_view = bbox_pred.view(bbox_pred.size(0), int(bbox_pred.size(1) / 4), 4)
@@ -95,24 +94,24 @@ class _fasterRKNN(nn.Module):
             bbox_pred = bbox_pred_select.squeeze(1)
 
         # compute object classification probability
-        cls_score = self.RCNN_cls_score(pooled_feat)
+        cls_score = self.RKNN_cls_score(pooled_feat)
         cls_prob = F.softmax(cls_score)
 
-        RCNN_loss_cls = 0
-        RCNN_loss_bbox = 0
+        RKNN_loss_cls = 0
+        RKNN_loss_bbox = 0
 
         if self.training:
             # classification loss
-            RCNN_loss_cls = F.cross_entropy(cls_score, rois_label)
+            RKNN_loss_cls = F.cross_entropy(cls_score, rois_label)
 
             # bounding box regression L1 loss
-            RCNN_loss_bbox = _smooth_l1_loss(bbox_pred, rois_target, rois_inside_ws, rois_outside_ws)
+            RKNN_loss_bbox = _smooth_l1_loss(bbox_pred, rois_target, rois_inside_ws, rois_outside_ws)
 
 
         cls_prob = cls_prob.view(batch_size, rois.size(1), -1)
         bbox_pred = bbox_pred.view(batch_size, rois.size(1), -1)
 
-        return rois, cls_prob, bbox_pred, rpn_loss_cls, rpn_loss_bbox, RCNN_loss_cls, RCNN_loss_bbox, rois_label
+        return rois, cls_prob, bbox_pred, rpn_loss_cls, rpn_loss_bbox, RKNN_loss_cls, RKNN_loss_bbox, rois_label
 
     def _init_weights(self):
         def normal_init(m, mean, stddev, truncated=False):
@@ -126,11 +125,11 @@ class _fasterRKNN(nn.Module):
                 m.weight.data.normal_(mean, stddev)
                 m.bias.data.zero_()
 
-        normal_init(self.RCNN_rpn.RPN_Conv, 0, 0.01, cfg.TRAIN.TRUNCATED)
-        normal_init(self.RCNN_rpn.RPN_cls_score, 0, 0.01, cfg.TRAIN.TRUNCATED)
-        normal_init(self.RCNN_rpn.RPN_bbox_pred, 0, 0.01, cfg.TRAIN.TRUNCATED)
-        normal_init(self.RCNN_cls_score, 0, 0.01, cfg.TRAIN.TRUNCATED)
-        normal_init(self.RCNN_bbox_pred, 0, 0.001, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RKNN_rpn.RPN_Conv, 0, 0.01, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RKNN_rpn.RPN_cls_score, 0, 0.01, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RKNN_rpn.RPN_bbox_pred, 0, 0.01, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RKNN_cls_score, 0, 0.01, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RKNN_bbox_pred, 0, 0.001, cfg.TRAIN.TRUNCATED)
 
     def create_architecture(self):
         self._init_modules()
--- lib/model/faster_rcnn/kerv_resnet.py
+++ lib/model/faster_rcnn/kerv_resnet.py
@@ -3,8 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 from model.utils.config import cfg
-from model.faster_rcnn.faster_rcnn import _fasterRCNN
-from model.kervnet.kervolution import kerv
+from model.faster_rcnn.faster_rknn import _fasterRKNN
+from model.kervolution.kervolution import Kerv2d
 
 import torch
 import torch.nn as nn
@@ -31,16 +31,20 @@ def conv3x3(in_planes, out_planes, stride=1):
   return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
            padding=1, bias=False)
 
+def kerv3x3(in_planes, out_planes, stride=1):
+  "3x3 convolution with padding"
+  return nn.Kerv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+           padding=1, bias=False)
 
 class BasicBlock(nn.Module):
   expansion = 1
 
   def __init__(self, inplanes, planes, stride=1, downsample=None):
     super(BasicBlock, self).__init__()
-    self.conv1 = conv3x3(inplanes, planes, stride)
+    self.conv1 = kerv3x3(inplanes, planes, stride)
     self.bn1 = nn.BatchNorm2d(planes)
     self.relu = nn.ReLU(inplace=True)
-    self.conv2 = conv3x3(planes, planes)
+    self.conv2 = kerv3x3(planes, planes)
     self.bn2 = nn.BatchNorm2d(planes)
     self.downsample = downsample
     self.stride = stride
@@ -69,12 +73,12 @@ class Bottleneck(nn.Module):
 
   def __init__(self, inplanes, planes, stride=1, downsample=None):
     super(Bottleneck, self).__init__()
-    self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False) # change
+    self.conv1 = nn.Kerv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False) # change
     self.bn1 = nn.BatchNorm2d(planes)
-    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, # change
+    self.conv2 = nn.Kerv2d(planes, planes, kernel_size=3, stride=1, # change
                  padding=1, bias=False)
     self.bn2 = nn.BatchNorm2d(planes)
-    self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+    self.conv3 = nn.Kerv2d(planes, planes * 4, kernel_size=1, bias=False)
     self.bn3 = nn.BatchNorm2d(planes * 4)
     self.relu = nn.ReLU(inplace=True)
     self.downsample = downsample
@@ -107,7 +111,7 @@ class kerv_ResNet(nn.Module):
   def __init__(self, block, layers, num_classes=1000):
     self.inplanes = 64
     super(kerv_ResNet, self).__init__()
-    self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+    self.conv1 = nn.Kerv2d(3, 64, kernel_size=7, stride=2, padding=3,
                  bias=False)
     self.bn1 = nn.BatchNorm2d(64)
     self.relu = nn.ReLU(inplace=True)
@@ -122,7 +126,7 @@ class kerv_ResNet(nn.Module):
     self.fc = nn.Linear(512 * block.expansion, num_classes)
 
     for m in self.modules():
-      if isinstance(m, nn.Conv2d):
+      if isinstance(m, nn.Kerv2d):
         n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
         m.weight.data.normal_(0, math.sqrt(2. / n))
       elif isinstance(m, nn.BatchNorm2d):
@@ -133,7 +137,7 @@ class kerv_ResNet(nn.Module):
     downsample = None
     if stride != 1 or self.inplanes != planes * block.expansion:
       downsample = nn.Sequential(
-        nn.Conv2d(self.inplanes, planes * block.expansion,
+        nn.Kerv2d(self.inplanes, planes * block.expansion,
               kernel_size=1, stride=stride, bias=False),
         nn.BatchNorm2d(planes * block.expansion),
       )
@@ -218,14 +222,14 @@ def resnet152(pretrained=False):
     model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
   return model
 
-class resnet(_fasterRCNN):
+class resnet(_fasterRKNN):
   def __init__(self, classes, num_layers=101, pretrained=False, class_agnostic=False):
     self.model_path = 'data/pretrained_model/resnet101_caffe.pth'
     self.dout_base_model = 1024
     self.pretrained = pretrained
     self.class_agnostic = class_agnostic
 
-    _fasterRCNN.__init__(self, classes, class_agnostic)
+    _fasterRKNN.__init__(self, classes, class_agnostic)
 
   def _init_modules(self):
     resnet = resnet101()
@@ -236,54 +240,54 @@ class resnet(_fasterRCNN):
       resnet.load_state_dict({k:v for k,v in state_dict.items() if k in resnet.state_dict()})
 
     # Build resnet.
-    self.RCNN_base = nn.Sequential(resnet.conv1, resnet.bn1,resnet.relu,
+    self.RKNN_base = nn.Sequential(resnet.conv1, resnet.bn1,resnet.relu,
       resnet.maxpool,resnet.layer1,resnet.layer2,resnet.layer3)
 
-    self.RCNN_top = nn.Sequential(resnet.layer4)
+    self.RKNN_top = nn.Sequential(resnet.layer4)
 
-    self.RCNN_cls_score = nn.Linear(2048, self.n_classes)
+    self.RKNN_cls_score = nn.Linear(2048, self.n_classes)
     if self.class_agnostic:
-      self.RCNN_bbox_pred = nn.Linear(2048, 4)
+      self.RKNN_bbox_pred = nn.Linear(2048, 4)
     else:
-      self.RCNN_bbox_pred = nn.Linear(2048, 4 * self.n_classes)
+      self.RKNN_bbox_pred = nn.Linear(2048, 4 * self.n_classes)
 
     # Fix blocks
-    for p in self.RCNN_base[0].parameters(): p.requires_grad=False
-    for p in self.RCNN_base[1].parameters(): p.requires_grad=False
+    for p in self.RKNN_base[0].parameters(): p.requires_grad=False
+    for p in self.RKNN_base[1].parameters(): p.requires_grad=False
 
     assert (0 <= cfg.RESNET.FIXED_BLOCKS < 4)
     if cfg.RESNET.FIXED_BLOCKS >= 3:
-      for p in self.RCNN_base[6].parameters(): p.requires_grad=False
+      for p in self.RKNN_base[6].parameters(): p.requires_grad=False
     if cfg.RESNET.FIXED_BLOCKS >= 2:
-      for p in self.RCNN_base[5].parameters(): p.requires_grad=False
+      for p in self.RKNN_base[5].parameters(): p.requires_grad=False
     if cfg.RESNET.FIXED_BLOCKS >= 1:
-      for p in self.RCNN_base[4].parameters(): p.requires_grad=False
+      for p in self.RKNN_base[4].parameters(): p.requires_grad=False
 
     def set_bn_fix(m):
       classname = m.__class__.__name__
       if classname.find('BatchNorm') != -1:
         for p in m.parameters(): p.requires_grad=False
 
-    self.RCNN_base.apply(set_bn_fix)
-    self.RCNN_top.apply(set_bn_fix)
+    self.RKNN_base.apply(set_bn_fix)
+    self.RKNN_top.apply(set_bn_fix)
 
   def train(self, mode=True):
     # Override train so that the training mode is set as we want
     nn.Module.train(self, mode)
     if mode:
       # Set fixed blocks to be in eval mode
-      self.RCNN_base.eval()
-      self.RCNN_base[5].train()
-      self.RCNN_base[6].train()
+      self.RKNN_base.eval()
+      self.RKNN_base[5].train()
+      self.RKNN_base[6].train()
 
       def set_bn_eval(m):
         classname = m.__class__.__name__
         if classname.find('BatchNorm') != -1:
           m.eval()
 
-      self.RCNN_base.apply(set_bn_eval)
-      self.RCNN_top.apply(set_bn_eval)
+      self.RKNN_base.apply(set_bn_eval)
+      self.RKNN_top.apply(set_bn_eval)
 
   def _head_to_tail(self, pool5):
-    fc7 = self.RCNN_top(pool5).mean(3).mean(2)
+    fc7 = self.RKNN_top(pool5).mean(3).mean(2)
     return fc7
